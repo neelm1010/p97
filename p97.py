38dd20f4e6f369ff8d873f38477fd88c8e26bd74
neelm1010 <69491453+neelm1010@users.noreply.github.com>
2021-12-15 17:48:28 -0500

    Add files via upload

---
+++ p97.py
@@ -0,0 +1,8 @@
+number=int (input("what number comes between 1 and 5")) 
+print(number)
+if(number<5):
+    print("more than 5 is wrong")
+elif(number>1):
+    print("less than 1 is wrong")
+else:
+    print("2,3 and 4 comes between 1 and 5")
